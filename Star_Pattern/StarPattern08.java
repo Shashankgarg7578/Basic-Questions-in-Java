f69fa734b9d1d1015a9885075da5d0a0760c1b27
Shashank Garg <47049430+Shashankgarg7578@users.noreply.github.com>
2021-04-20 07:35:14 -0400

    Add files via upload

---
+++ Star_Pattern/StarPattern08.java
@@ -0,0 +1,23 @@
+package Star_Pattern;
+
+public class StarPattern08
+{
+ public static void main(String[] args)
+ {
+   for(int i=1 ; i<=4 ; i++)
+   {
+	   for(int j =1 ; j<=7 ; j++)
+	   {
+		   if(j>=i && j<=8-i)
+		   {
+			   System.out.print("*");
+		   }
+		   else
+		   {
+			   System.out.print(" ");
+		   }
+	   }
+	   System.out.println();
+   }
+ }
+}
