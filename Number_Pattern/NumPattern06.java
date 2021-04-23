155f4e5e5edc326aa663a3034f1969f6410b535b
Shashank Garg <47049430+Shashankgarg7578@users.noreply.github.com>
2021-04-23 01:53:43 -0400

    Add files via upload

---
+++ Number_Pattern/NumPattern06.java
@@ -0,0 +1,25 @@
+package Number_Pattern;
+
+public class NumPattern06 
+{
+ public static void main(String[] args) 
+ {
+  for(int i=1;i<=5;i++)
+  {
+	  int k=0;
+	  for(int j=1;j<=5;j++)
+	  {
+		  if(j<=i)
+		  {
+			  System.out.print(k+" ");
+			  k=k+i-1;
+		  }
+		  else
+		  {
+			  System.out.print(" ");
+		  }
+	  }
+	System.out.println();  
+  }
+ }
+}
