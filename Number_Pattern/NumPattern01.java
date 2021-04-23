71f5aa67b49e9582f867b9df5b28c88e185d72d6
Shashank Garg <47049430+Shashankgarg7578@users.noreply.github.com>
2021-04-23 01:52:02 -0400

    Add files via upload

---
+++ Number_Pattern/NumPattern01.java
@@ -0,0 +1,32 @@
+package Number_Pattern;
+
+public class NumPattern01 
+{
+ public static void main(String[] args) 
+ {
+	 int k ;
+	
+   	for(int i =1; i<=4;i++)
+   	{
+   		k=1;
+  
+   		for(int j = 1; j<=7;j++)
+   		{
+   			if(j>=5-i && j<=3+i)
+   			{
+   				System.out.print(k);
+   				
+  
+   				k++;
+   				
+   			}
+   			else
+   			{
+   				System.out.print(" ");
+   
+   			}
+   		}
+   		System.out.println();
+   	}
+ }
+}
