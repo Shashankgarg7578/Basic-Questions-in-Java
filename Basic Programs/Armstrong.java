42fd84f5d399be6780f75bbc2e0790989b391e1e
Shashank Garg <47049430+Shashankgarg7578@users.noreply.github.com>
2021-05-08 09:45:44 -0400

    Add files via upload

---
+++ Basic Programs/Armstrong.java
@@ -0,0 +1,42 @@
+public class Armstrong
+{
+  public static void main(String[] args) {
+	
+    int no = 153;
+    int t1 = no;
+    int leng = 0;
+    while(t1!=0)
+    {
+      leng = leng +1;
+      t1 = t1 /10; 
+    }
+
+   int t2 = no;
+   int arm = 0;
+
+   while(t2!=0)
+   {
+     int mul = 1;
+
+     int  rem = t2%10 ; 
+
+     for(int i =0 ; i<leng ; i++)
+     {
+       mul = mul * rem;
+     }
+     arm = arm +mul;
+    t2 = t2 / 10;
+    }
+
+   if(no==arm)
+   {
+	   System.out.println("Amrstrong no");
+   }
+   else
+   {
+	   System.out.println("It is not");
+   }
+   
+   
+  }
+}
